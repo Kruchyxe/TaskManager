c2634a5f7d8287912fc7ac10e977625cb739311c
Kruchyxe <ragamuffin@o2.pl>
2020-10-16 11:56:45 +0200

    next step: load data from file

---
+++ src/README.md
@@ -0,0 +1,10 @@
+# TaskMenager
+Simple console Task Menager (Java)
+
+## Description
+1.  You need to run your IDE and run this program from your machine. 
+2.  You must import JAR files ex. commons-lang3-3.9.jar
+3.  Import class ConsoleColors
+4.  Then run your aplication
+
+
---
+++ src/pl/coderslab/TaskManagerSimple.java
@@ -0,0 +1,64 @@
+package pl.coderslab;
+
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class TaskManagerSimple {
+    static final String FILE_NAME = "tasks.csv";
+    static final String[] OPTIONS = {"add", "remove", "list", "exit"};
+    static String[][] tasks;
+
+
+    public static void main(String[] args) {
+        tasks = loadDataToTab(FILE_NAME);
+        printOptions(OPTIONS);
+
+
+    }
+
+    public static void printOptions(String[] tab) {
+
+        System.out.println(ConsoleColors.BLUE);
+        System.out.println("Please select an option: " + ConsoleColors.RESET);
+        for (String option : tab) {
+            System.out.println(option);
+
+        }
+    }
+
+    public static String[][] loadDataToTab(String fileName) {
+        Path dir = Paths.get(fileName);
+        if (!Files.exists(dir)) {
+            /* If directory not exist you will receive this message
+             * important create file */
+            System.out.println("File not exist.");
+            System.exit(0);
+        }
+
+        String[][] tab = null;
+
+        try {
+            List<String> strings = Files.readAllLines(dir);
+            tab = new String[strings.size()][strings.get(0).split(",").length];
+            for (int i = 0; i < strings.size(); i++) {
+                String[] split = strings.get(i).split(",");
+                for (int j = 0; j < split.length; j++) {
+
+                    tab[i][j] = split[j];
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return tab;
+
+    }
+
+}
+
+
+
