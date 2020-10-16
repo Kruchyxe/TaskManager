8af86b54267794cf55e0e448cb0fa2b5daa5ae75
Kruchyxe <ragamuffin@o2.pl>
2020-10-16 11:02:12 +0200

    Creating class and main method

---
+++ src/pl/coderslab/TaskManagerSimple.java
@@ -0,0 +1,28 @@
+package pl.coderslab;
+
+
+public class TaskManagerSimple {
+    static final String FILE_NAME = "tasks.csv";
+    static final String[] OPTIONS = {"add", "remove", "list", "exit"};
+    static String[][] tasks;
+
+
+    public static void main(String[] args) {
+        printOptions(OPTIONS);
+
+
+    }
+
+    public static void printOptions(String[] tab) {
+
+        System.out.println(ConsoleColors.BLUE);
+        System.out.println("Please select an option: " + ConsoleColors.RESET);
+        for (String option : tab) {
+            System.out.println(option);
+
+        }
+
+    }
+
+}
+
