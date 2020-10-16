c70245f80b74e6277addbfc8d461d74075c3c717
Kruchyxe <ragamuffin@o2.pl>
2020-10-16 13:32:39 +0200

    ending step: rest methods

--- src/pl/coderslab/TaskManagerSimple.java
+++ src/pl/coderslab/TaskManagerSimple.java
@@ -8,6 +8,8 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.Scanner;
 
+import static java.lang.System.arraycopy;
+
 public class TaskManagerSimple {
     static final String FILE_NAME = "tasks.csv";
     static final String[] OPTIONS = {"add", "remove", "list", "exit"};
@@ -24,7 +26,12 @@ public class TaskManagerSimple {
 
             switch (input) {
                 case "exit":
+                    saveTabToFile(FILE_NAME, tasks);
+                    System.out.println(ConsoleColors.RED + "Bye, bye.");
+                    System.exit(0);
                 case "remove":
+                    removeTask(tasks, getTheNumber());
+                    System.out.println("Value was successfully deleted.");
                     break;
                 case "add":
                     break;
@@ -67,10 +74,7 @@ public class TaskManagerSimple {
             tab = new String[strings.size()][strings.get(0).split(",").length];
             for (int i = 0; i < strings.size(); i++) {
                 String[] split = strings.get(i).split(",");
-                for (int j = 0; j < split.length; j++) {
-
-                    tab[i][j] = split[j];
-                }
+                arraycopy(split, 0, tab[i], 0, split.length);
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -87,6 +91,65 @@ public class TaskManagerSimple {
                 System.out.print(tab[i][j] + " ");
             }
             System.out.println();
+        }
+    }
+
+    private static void addTask(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please add task description");
+        String description = scanner.nextLine();
+        System.out.println("Please add task due date");
+        String dueDate = scanner.nextLine();
+        System.out.println("Is your task is important: true/false");
+        String isImportant = scanner.nextLine();
+        tasks =  Arrays.copyOf(tasks, tasks.length + 1);
+
+        tasks[tasks.length - 1] = new String[3];
+        tasks[tasks.length - 1][0] = description;
+        tasks[tasks.length - 1][1] = dueDate;
+        tasks[tasks.length - 1][2] = isImportant;
+    }
+
+    public static boolean isNumberGreaterEqualZero(String input) {
+        if (NumberUtils.isParsable(input)) {
+            return Integer.parseInt(input) >= 0;
+        }
+        return false;
+    }
+
+    public static int getTheNumber() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please select number to remove.");
+        String n = scanner.nextLine();
+        while (!isNumberGreaterEqualZero(n)) {
+            System.out.println("Incorrect argument passed. Please give number greater or equal 0");
+            scanner.nextLine();
+        }
+        return Integer.parseInt(n);
+    }
+
+    private static void removeTask(String[][] tab, int index) {
+        try {
+            if (index < tab.length) {
+                tasks = ArrayUtils.remove(tab, index);
+            }
+        } catch (ArrayIndexOutOfBoundsException ex) {
+            System.out.println("Element not exist in tab");
+        }
+
+    }
+    public static void saveTabToFile(String fileName, String[][] tab) {
+
+        Path dir = Paths.get(fileName);
+        String[] lines = new String[tasks.length];
+        for (int i = 0; i < tab.length; i++) {
+            lines[i] = String.join(",", tab[i]);
+        }
+
+        try {
+            Files.write(dir, Arrays.asList(lines));
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
     }
 
