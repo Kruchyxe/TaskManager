4bb86e0badabee1a727b95140842ffcd788e27a6
Kruchyxe <ragamuffin@o2.pl>
2020-10-16 11:18:28 +0200

    next step: load data from file

--- src/pl/coderslab/TaskManagerSimple.java
+++ src/pl/coderslab/TaskManagerSimple.java
@@ -1,6 +1,12 @@
 package pl.coderslab;
 
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class TaskManagerSimple {
     static final String FILE_NAME = "tasks.csv";
     static final String[] OPTIONS = {"add", "remove", "list", "exit"};
@@ -8,6 +14,7 @@ public class TaskManagerSimple {
 
 
     public static void main(String[] args) {
+        tasks = loadDataToTab(FILE_NAME);
         printOptions(OPTIONS);
 
 
@@ -21,8 +28,37 @@ public class TaskManagerSimple {
             System.out.println(option);
 
         }
+    }
+
+    public static String[][] loadDataToTab(String fileName) {
+        Path dir = Paths.get(fileName);
+        if (!Files.exists(dir)) {
+            /* If directory not exist you will receive this message
+             * important create file */
+            System.out.println("File not exist.");
+            System.exit(0);
+        }
+
+        String[][] tab = null;
+
+        try {
+            List<String> strings = Files.readAllLines(dir);
+            tab = new String[strings.size()][strings.get(0).split(",").length];
+            for (int i = 0; i < strings.size(); i++) {
+                String[] split = strings.get(i).split(",");
+                for (int j = 0; j < split.length; j++) {
+
+                    tab[i][j] = split[j];
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return tab;
 
     }
 
 }
+
+
 
